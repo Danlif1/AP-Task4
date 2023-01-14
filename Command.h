3175bb9667affb497719363c847592d92bcf8bde
Danlif1 <68195849+Danlif1@users.noreply.github.com>
2023-01-14 18:59:36 +0200

    Create Command.h
    The base implementation of all the commands.

---
+++ Command.h
@@ -0,0 +1,37 @@
+//
+// Created by Daniel Lifshitz on 14/01/2023.
+//
+
+#ifndef AP_TASK4_COMMAND_H
+#define AP_TASK4_COMMAND_H
+
+
+#include <string>
+#include "Info.h"
+#include "DefaultIO.h"
+
+class Command {
+private:
+    Info& ctx;
+    DefaultIO& dio;
+protected:
+    std::string description; // description in the menu.
+    std::string instruction; // the message shown after the command is chosen.
+    Info &getCtx();
+
+    DefaultIO &getDio() const;
+public:
+    Command(Info& ctx, DefaultIO &dio);
+    virtual void execute() = 0;
+    virtual ~Command();
+    virtual void printDesc();
+    virtual void printInst();
+
+    const std::string &getDescription() const;
+
+    const std::string &getInstruction() const;
+
+};
+
+
+#endif //AP_TASK4_COMMAND_H

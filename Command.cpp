3d184f0d8b2732b6c6ea144dcd028b56ec6351f5
Danlif1 <68195849+Danlif1@users.noreply.github.com>
2023-01-14 19:00:20 +0200

    Create Command.cpp
    the implementation of the Command.h file

---
+++ Command.cpp
@@ -0,0 +1,73 @@
+//
+// Created by Daniel Lifshitz on 14/01/2023.
+//
+
+
+#include "Command.h"
+
+
+/**
+ * @brief Printing the description.
+ * 
+ */
+void Command::printDesc() {
+    dio.write(description);
+}
+
+/**
+ * @brief Printing the instructions.
+ * 
+ */
+void Command::printInst() {
+    dio.write(instruction);
+}
+
+/**
+ * @brief Construct a new Command:: Command object.
+ * 
+ * @param ctx Things the command needs to know in order to work.
+ * @param dio DefaultIO used.
+ */
+Command::Command(Info& ctx, DefaultIO& dio): ctx(ctx), dio(dio) {}
+
+/**
+ * @brief Getter for the description.
+ * 
+ * @return const std::string& The description.
+ */
+const std::string &Command::getDescription() const {
+    return description;
+}
+
+/**
+ * @brief Getter for the instructions.
+ * 
+ * @return const std::string& The instructions.
+ */
+const std::string &Command::getInstruction() const {
+    return instruction;
+}
+
+/**
+ * @brief Getter for the Info.
+ * 
+ * @return Info& The Info.
+ */
+Info &Command::getCtx() {
+    return ctx;
+}
+
+/**
+ * @brief Getter for the defaultIO.
+ * 
+ * @return DefaultIO& The defaultIO.
+ */
+DefaultIO &Command::getDio() const {
+    return dio;
+}
+
+/**
+ * @brief Destroy the Command:: Command object.
+ * 
+ */
+Command::~Command() = default;

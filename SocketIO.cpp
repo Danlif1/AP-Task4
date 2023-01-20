cfc399306595c0ccd351dc6a624b769014330f88
awesome0597 <adira.weiss@gmail.com>
2023-01-20 12:29:39 +0200

    Added new members: buffer and client_socket, so that it can be used by both client and server. Implemented read and write functions added a helper function sendTO

--- SocketIO.cpp
+++ SocketIO.cpp
@@ -3,17 +3,42 @@
 //
 
 #include "SocketIO.h"
+#include <iostream>
+#include <sys/socket.h>
+#include <stdio.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <cstring>
+#include <string>
 
 void SocketIO::write(double d) {
 //send to client
 }
 
 void SocketIO::write(std::string text) {
-//send to client
+    if (SocketIO::sendTo(text) < 0) {
+        perror("error sending message");
+    }
+}
+
+int SocketIO::sendTo(const std::string& text) {
+
+    return send(this->client_socket, text.c_str(), text.size() + 1, 0);
 }
 
 std::string SocketIO::read() {
-    //read from client
+    memset(&(buffer), 0, sizeof(buffer));
+    int expected_data_len = sizeof(buffer);
+    int read_bytes = recv(this->client_socket, buffer, expected_data_len, 0);
+    if (read_bytes < 0) {
+        std::perror("error reading from socket");
+    } else if (read_bytes == 0) {
+        // if connection to server is closed we end the program
+        std::perror("server disconnected");
+    } else {
+        return std::string(buffer);
+    }
 }
 
 
@@ -24,10 +49,10 @@ void SocketIO::sendFile(std::fstream &file_s, long file_size) {
     long left = file_size;
     long sum = 0;
     char buffer[CHUNK_SIZE];
-    while(left > 0) {
-        long sending = (CHUNK_SIZE<left)? CHUNK_SIZE: left;
+    while (left > 0) {
+        long sending = (CHUNK_SIZE < left) ? CHUNK_SIZE : left;
         file_s.read(buffer, sending);
-        sent = send(client_sock, buffer, sending, 0);
+        sent = send(this->client_socket, buffer, sending, 0);
         if (sent < 0) {
             std::cout << "Server : Error sending file." << std::endl;
         } else {
--- SocketIO.h
+++ SocketIO.h
@@ -8,14 +8,22 @@
 
 #include "DefaultIO.h"
 
-class SocketIO: public DefaultIO {
+class SocketIO : public DefaultIO {
+    int client_socket;
+
+    int sendTo(const std::string& text);
+
+    char buffer[4096];
+public:
+    SocketIO(int client_socket) : client_socket(client_socket) {};
+
     void write(double d) override;
+
     void write(std::string text) override;
-    void sendFile(std::fstream& file_s, long file_size) override; //full file stream
+
+    void sendFile(std::fstream &file_s, long file_size); //full file stream
     std::string read() override;
 };
-
-
 
 
 #endif //AP_4_SOCKETIO_H

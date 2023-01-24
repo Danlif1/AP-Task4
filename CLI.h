f7cb6b00ce54e966008a45c1204f6127ee2d305c
awesome0597 <adira.weiss@gmail.com>
2023-01-24 20:54:01 +0200

    Added working CLI

---
+++ CLI.h
@@ -0,0 +1,35 @@
+//
+// Created by lenovo on 1/17/2023.
+//
+
+#ifndef AP_TASK4_CLI_H
+#define AP_TASK4_CLI_H
+
+#include "DefaultIO.h"
+#include "Command.h"
+#include "UploadFile.h"
+#include "AlgorithmSettings.h"
+#include "ClassifyData.h"
+#include "DisplayResult.h"
+#include "DownloadResult.h"
+#include <map>
+
+
+class CLI {
+    std::map<int,Command *> commands;
+    DefaultIO* dio;
+    KNN* knn;
+    bool should_continue;
+    void printMenu();
+    void waitForInput();
+    bool isValidInput(std::string input);
+public:
+    CLI(DefaultIO* dio, KNN* knn);
+
+    void start();
+
+    ~CLI() = default;
+};
+
+
+#endif //AP_TASK4_CLI_H
--- main.cpp
+++ main.cpp
@@ -4,23 +4,32 @@
 #include "AlgorithmSettings.h"
 #include "SocketIO.h"
 #include "StandardIO.h"
-//#include "CLI.h"
+#include "CLI.h"
 #include "ClassifyData.h"
+#include  "DisplayResult.h"
+#include <vector>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <cstring>
+#include <fstream>
 
 
-
+void read_file(std::string& file_name){
+    std::ifstream fin;
+    fin.open(file_name);
+    if(!fin.is_open()) {
+        //Error Message
+        std::cout<< "Could not open the file " << file_name << std::endl;
+        std::cout<<"Error code: " << strerror(errno) << std::endl;
+    }
+}
 int main() {
 KNN* knn = new KNN();
 //SocketIO* socketIo = new SocketIO(1);
 StandardIO* standardIo = new StandardIO();
-AlgorithmSettings* algorithmSettings = new AlgorithmSettings(standardIo, knn);
-ClassifyData* classifyData = new ClassifyData(standardIo, knn);
-
-while (true) {
-    algorithmSettings->execute();
-//    classifyData->execute();
-
-}
+CLI* cli = new CLI(standardIo, knn);
+cli->start();
 
     return 0;
 }

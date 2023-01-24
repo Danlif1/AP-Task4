a058ce102011fed042fcc8ab1d10f34bd372d6fa
awesome0597 <adira.weiss@gmail.com>
2023-01-24 19:31:57 +0200

    Added command 1

---
+++ UploadFile.cpp
@@ -0,0 +1,75 @@
+//
+// Created by Daniel Lifshitz on 18/01/2023.
+//
+
+#include "UploadFile.h"
+#include "SafetyChecks.h"
+
+UploadFile::UploadFile(DefaultIO* dio, KNN* knn) {
+    description = "1. upload an unclassified csv file\n";
+    instruction = "Please upload your local train CSV file.";
+    insturction_2 = "Please upload your local test CSV file.";
+}
+
+bool UploadFile::isFileGood(std::vector<Point> input) {
+    if(IsSameSize(input) && !input.empty()){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+std::vector<Point> UploadFile::getClassifiedFromClient() {
+    std::vector<Point> data;
+    std::string word;
+    std::vector<std::string> elems;
+    std::string line = dio->read();
+    while (line != "done") {
+        std::stringstream ss(line);
+        std::string item;
+        while (std::getline(ss, item, ',')) {
+            elems.push_back(item);
+        }
+        Point p;
+        p.setFromVector(elems);
+        data.push_back(p);
+        line = dio->read();
+    }
+    return data;
+}
+
+std::vector<Point> UploadFile::getUnclassifiedFromClient() {
+    std::vector<Point> data;
+    std::string line = dio->read();
+    while (line != "done") {
+        Point p;
+        p.setFromString(line, ',');
+        data.push_back(p);
+        line = dio->read();
+    }
+    return data;
+}
+
+void UploadFile::execute() {
+    dio->write(this->instruction);
+    std::vector<Point> classifiedPoints = getClassifiedFromClient();
+    dio->write("Upload complete.");
+    if(!isFileGood(classifiedPoints)){
+        dio->write("Upload failed.");
+    } else {
+        dio->write(this->insturction_2);
+        std::vector<Point> unclassifiedPoints = getUnclassifiedFromClient();
+        if (isFileGood(unclassifiedPoints) &&
+        unclassifiedPoints[0].getAll().size() == classifiedPoints[0].getAll().size()) {
+            knn->fit(classifiedPoints);
+            knn->fit_unclassified(unclassifiedPoints);
+            dio->write("Upload complete.");
+        } else {
+            dio->write("Upload failed.");
+        }
+    }
+
+
+}
+
+
---
+++ UploadFile.h
@@ -0,0 +1,38 @@
+//
+// Created by Daniel Lifshitz on 18/01/2023.
+//
+
+#ifndef AP_TASK4_UPLOADFILE_H
+#define AP_TASK4_UPLOADFILE_H
+
+#include "Command.h"
+#include <iostream>
+#include <sys/socket.h>
+#include <cstdio>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <cstring>
+#include <fstream>
+#include <unistd.h>
+#include "PointReader.h"
+#include "Point.h"
+
+
+class UploadFile : public Command {
+private:
+    std::string insturction_2;
+    std::vector<Point> getClassifiedFromClient();
+    std::vector<Point> getUnclassifiedFromClient();
+    bool is_file_good = true;
+    bool isFileGood(std::vector<Point> input);
+
+public:
+    UploadFile(DefaultIO* dio, KNN* knn);
+    const std::string &getInstruction(int);
+    void execute() override;
+};
+
+
+
+#endif //AP_TASK4_UPLOADFILE_H

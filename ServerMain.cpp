e8a2609e91598e0cbae82aa07fb76f18d2f383db
Danlif1 <68195849+Danlif1@users.noreply.github.com>
2023-01-27 21:38:28 +0200

    Update ServerMain.cpp
    added validity check for port.

--- ServerMain.cpp
+++ ServerMain.cpp
@@ -4,19 +4,37 @@
 #include <string.h>
 #include <pthread.h>
 #include "Server.h"
+#include <regex>
 
 /**
  * @brief The main function of the server
- * 
+ *
  * @param argc Should have file and port by that order.
  * @param argv Should be 3.
  * @return int Returning 0 if there is a problem with the inputs.
  */
-int main(int argc, char const* argv[]) {
+bool isValidPort(std::string port) {
+    for (int i = 0; i < port.size(); ++i) {
+        if (!isdigit(port[i])) {
+            return false;
+        }
+    }
+    if (std::stoi(port) < 0 || std::stoi(port) > 65535) {
+        return false;
+    }
+    return true;
+}
 
-    Server server = Server(atoi(argv[1]));
-    server.connectToClient();
-    server.closeSocket();
-    delete &server;
-    return 0;
+int main(int argc, char const* argv[]) {
+    if (isValidPort(argv[1])){
+        Server server = Server(atoi(argv[1]));
+        server.connectToClient();
+        server.closeSocket();
+        server.~Server();
+        return 0;
+    } else {
+        std::cout << "Invalid port" << std::endl;
+        return 0;
+    }
+
 }

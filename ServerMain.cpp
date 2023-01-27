bc91bac156c8a49cc5bd13bf154efc75b2de512d
awesome0597 <adira.weiss@gmail.com>
2023-01-27 19:11:17 +0200

    changed delete to destructor and added port check

--- ServerMain.cpp
+++ ServerMain.cpp
@@ -4,6 +4,7 @@
 #include <string.h>
 #include <pthread.h>
 #include "Server.h"
+#include <regex>
 
 /**
  * @brief The main function of the server
@@ -12,11 +13,29 @@
  * @param argv Should be 3.
  * @return int Returning 0 if there is a problem with the inputs.
  */
+bool isValidPort(std::string port) {
+    for (int i = 0; i < port.size(); ++i) {
+        if (!isdigit(port[i])) {
+            return false;
+        }
+    }
+    if (std::stoi(port) < 0 || std::stoi(port) > 65535) {
+        return false;
+    }
+    return true;
+}
+
 int main(int argc, char const* argv[]) {
 
-    Server server = Server(atoi(argv[1]));
-    server.connectToClient();
-    server.closeSocket();
-    delete &server;
-    return 0;
+    if (isValidPort(argv[1])){
+        Server server = Server(atoi(argv[1]));
+        server.connectToClient();
+        server.closeSocket();
+        server.~Server();
+        return 0;
+    } else {
+        std::cout << "Invalid port" << std::endl;
+        return 0;
+    }
+
 }
